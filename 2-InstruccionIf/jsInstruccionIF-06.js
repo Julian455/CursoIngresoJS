aa76b34664de1683eba1c136a27bc8f620cd658f
Julian455 <98592697+Julian455@users.noreply.github.com>
2022-02-17 00:23:37 -0300

    Ejercicios de IF completados

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,41 +1,67 @@
 /*
-Julian Trasar Div E
-EJ if 6
-Al ingresar una edad debemos informar si la persona es mayor de edad (mas de 18 años) o adolescente (entre 13 y 17 años) o niño (menor a 13 años).
+Julian Trasar Div
+6 bis
 
+Ejercicio 6 bis(usar if else):
+Al ingresar por prompt una edad debemos informar si
+la persona es anciano(mas de 70), mayor de edad (entre 70 y 18 años),
+adolescente (entre 13 y 17 años), niño (menor a 13 años), bebe (0 a 3 años).
+mostrar por ID el resultado.
 */
+
+
+
 function mostrar()
-{
-	//declaramos variables
-	var edad;
+{           
+      //declaramos variables					  
+			var edad;
+			var mensaje;
 
-	//traemos edad por id
-	edad = document.getElementById("txtIdEdad").value;
 
-	//parseamos
-	edad=parseInt(edad);
+			//mostramos por prompt
+			edad=prompt("Ingrese su edad");
 
-	if(edad >= 18)
-	{
+			//parseamos
+			edad=parseInt(edad);
 
-		alert("usted es mayor de edad");
+			 if(edad >69)
+			 {
+			 		mensaje = "Usted es un anciano";
+			 }
+			 else
+			 {
+				 	if(edad<71 && edad >17)
+				 	{
+				 		mensaje = "Usted es mayor de edad";
+				 	}
+				 	else
+				 	{
+					 		if(edad>12 && edad <18)
+						 	{
+						 		mensaje = "Usted es adolecente";
+						 	}
+						 	else
+						 	{
+						 		if(edad > 3 && edad< 13)
+						 		{
+						 			mensaje = "Usted es un niño";
+						 		}
+						 		else
+						 		{
+                  mensaje = "Es un bebe";
+						 		}
 
-    }
+				 	 		}
+				 	}
+				 
+				 
+				 
+			 	}
 
-    else if(edad < 12)
+			 	document.getElementById("txtIdEdad").value = mensaje;
 
-    {
 
-      alert("usted es un nuño");
 
-    }
-
-    else
-
-    {
-
-      alert("usted es un adolescente"); 
-
-    }
-    
-     }
+}
+			  
+ 
--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -7,29 +7,51 @@ Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero",
 */
 function mostrar()
 {
-	//declaramos luna variable
-	var estadoCivil;
+  
+	//delcaramos variables
 	var edad;
-    
-    //traemos todo por id
-	estadoCivil=document.getElementById("estadoCivil").value;
+	var mensaje
+
+	//traemos todo por id
 	edad = document.getElementById("txtIdEdad").value;
-    
-    //parseamos
+	//estadoCivil = document.getElementById("estadoCivil").value;
+
+	//parseamos edad
 	edad=parseInt(edad);
 
-
-
-	if(edad < 18 && estadoCivil != "Soltero")
-
+	 if(edad <= 13)
 	{
-		alert("Es muy pequeño para No ser soltero");
+		mensaje = "Feliz dia"
 	}
-
-
-
+	else if(edad >13 && edad <18)
+	{
+		mensaje = "Usted es adolecente"
+	}
 	
+	else if(edad> 18)
+	{
+		mensaje = "Usted es mayor de edad "
+    
+		if(edad ==88)
+		{
+			mensaje= "lindo numero"
+		}
+		else if(edad == 33)
+		{
+			mensaje = "como cristo"
+		}
+		else if(edad > 59)
+		{
+			mensaje = "a jubilarse"
+		}
+		else {
+		mensaje = "Usted es mayor de edad "
 
-	
+		}
+	}
+		alert(mensaje);
+
 
 }//FIN DE LA FUNCIÓN
+
+
