f0218126688eee3ee51cc1e5cc5378788be0e31e
julian455 <julianttrasar@gmail.com>
2022-02-19 22:00:44 -0300

    Ejercicio 6

--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -1,3 +1,10 @@
+/*
+Julian Trasar Div E
+EJ while 6
+Al presionar el botón pedir 5 números 
+e informar la suma acumulada y el promedio.
+
+*/ 
 function mostrar()
 {
 	var contador;
@@ -6,8 +13,16 @@ function mostrar()
 
 	contador=0;
 	acumulador=0;
-	
+
+	while(contador<5)//condicion
+	{
+		numeroIngresado = prompt("ingrese 5 numeros");
+		numeroIngresado=parseInt(numeroIngresado);
+		acumulador = acumulador + numeroIngresado;
+
+		contador = contador + 1; //modifica nuestra condicion
+	}
 	
 	txtIdSuma.value=acumulador;
 	txtIdPromedio.value=acumulador/5;
-}//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCIÓN 
