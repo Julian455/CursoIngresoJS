c4fca58b69310e218da6fb4058c41d863f130f96
julian455 <julianttrasar@gmail.com>
2022-07-05 11:33:06 -0300

    completados

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,7 +5,7 @@ Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
-function sumar()
+/*function sumar()
 
 {	
 	//declaramos variables	var numUno;
@@ -99,10 +99,86 @@ function dividir()
 	alert(" la division es " + resultado);	
 
 }
-
+*/
 /*
 
 txtIdNumeroUno
 txtIdNumeroDos
 
 */
+
+/*
+Julian Trasar Div E 
+ejercicio 7
+Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt),realizar la operación correcta y 
+mostrar el resulto por medio de "ALERT"
+ej.: "la Resta es 750"*/ 
+function sumar()
+{
+	let numeroUno;
+	let numeroDos;
+	let resultado;
+
+		numeroUno=document.getElementById("txtIdNumeroUno").value;
+		numeroDos=document.getElementById("txtIdNumeroDos").value;
+
+		numeroUno=parseInt(numeroUno);
+		numeroDos=parseInt(numeroDos);
+
+		resultado=numeroUno + numeroDos;
+
+		alert("La suma es: " + resultado);;
+}
+
+function restar()
+{
+	let numeroUno;
+	let numeroDos;
+	let resultado;
+
+	numeroUno=document.getElementById("txtIdNumeroUno").value;
+	numeroDos=document.getElementById("txtIdNumeroDos").value;
+
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	resultado=numeroUno - numeroDos;
+
+	alert("La resta es: " + resultado);
+}
+function dividir()
+{
+
+	let numeroUno;
+	let numeroDos;
+	let resultado;
+
+	    numeroUno=document.getElementById("txtIdNumeroUno").value;
+		numeroDos=document.getElementById("txtIdNumeroDos").value;
+
+		numeroUno=parseInt(numeroUno);
+		numeroDos=parseInt(numeroDos);
+
+		resultado=numeroUno/numeroDos;
+
+		alert("La division es: " + resultado);
+
+
+}
+function multiplicar()
+{
+	let numeroUno;
+	let numeroDos;
+	let resultado;
+
+	    numeroUno=document.getElementById("txtIdNumeroUno").value;
+		numeroDos=document.getElementById("txtIdNumeroDos").value;
+
+		numeroUno=parseInt(numeroUno);
+		numeroDos=parseInt(numeroDos);
+
+		resultado=numeroUno * numeroDos;
+
+		alert("La multiplicacion es: " + resultado);
+}
